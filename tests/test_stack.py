d9f6e8ed77391a833bcce12878b864392cb6363a
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2022-12-10 23:21:30 +0100

    Add unit tests for stack implementations

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,7 +1,37 @@
 from unittest import TestCase
 
+from data_structures.stack.implementation import ArrayBasedStack, LinkedStack
 
-class TestStack(TestCase):
+
+class BaseTestStack(TestCase):
+
+    __test__ = False
 
     def setUp(self):
-        pass
+        self.stack = None
+        self.test_item = None
+
+    def test_empty_stack(self):
+        self.assertTrue(self.stack.is_empty())
+
+    def test_push_and_pop(self):
+        self.stack.push(self.test_item)
+        self.assertEqual(self.test_item, self.stack.pop())
+
+
+class TestArrayBaseStack(BaseTestStack):
+
+    __test__ = True
+
+    def setUp(self):
+        self.stack = ArrayBasedStack()
+        self.test_item = 5
+
+
+class TestLinkedStack(BaseTestStack):
+
+    __test__ = True
+
+    def setUp(self):
+        self.stack = LinkedStack()
+        self.test_item = 5
