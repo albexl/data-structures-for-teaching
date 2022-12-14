1a5efe8bd049586ae5118c2cd83d59402b5623ff
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2022-12-14 19:24:16 +0100

    Comply with some pylint suggestions

--- data_structures/priority_queues/moving_balls.py
+++ data_structures/priority_queues/moving_balls.py
@@ -1,8 +1,9 @@
 import random
+import sys
 import time
 
 import pygame
-from pygame.locals import *
+from pygame.locals import QUIT
 
 WIDTH = 400
 HEIGHT = 400
@@ -10,26 +11,26 @@ HEIGHT = 400
 
 class Ball():
 
-    def __init__(self, px, py, vx, vy, r):
-        self.px = px
-        self.py = py
-        self.vx = vx
-        self.vy = vy
-        self.r = r
+    def __init__(self, pos_x, pos_y, vel_x, vel_y, radius):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+        self.radius = radius
 
-    def move(self, t):
-        if self.px + self.vx * t < self.r or self.px + self.vx * t + self.r > HEIGHT:
-            self.vx = -self.vx
-        if self.py + self.vy * t < self.r or self.py + self.vy * t + self.r > HEIGHT:
-            self.vy = -self.vy
-        self.px += self.vx * t
-        self.py += self.vy * t
+    def move(self, dir_vect):
+        if self.pos_x + self.vel_x * dir_vect < self.radius or self.pos_x + self.vel_x * dir_vect + self.radius > HEIGHT:
+            self.vel_x = -self.vel_x
+        if self.pos_y + self.vel_y * dir_vect < self.radius or self.pos_y + self.vel_y * dir_vect + self.radius > HEIGHT:
+            self.vel_y = -self.vel_y
+        self.pos_x += self.vel_x * dir_vect
+        self.pos_y += self.vel_y * dir_vect
 
 
 def handle(event):
     if event.type == QUIT:
         pygame.quit()
-        quit()
+        sys.exit()
 
 
 if __name__ == '__main__':
@@ -42,16 +43,16 @@ if __name__ == '__main__':
 
     n = int(input('Enter amount of balls: '))
 
-    balls = [Ball(px=random.randrange(0, HEIGHT), py=random.randrange(
-        0, WIDTH), vx=10, vy=10, r=5) for _ in range(n)]
+    balls = [Ball(pos_x=random.randrange(0, HEIGHT), pos_y=random.randrange(
+        0, WIDTH), vel_x=10, vel_y=10, radius=5) for _ in range(n)]
 
     while True:
-        for event in pygame.event.get():
-            handle(event)
+        for game_event in pygame.event.get():
+            handle(game_event)
         game_display.fill((255, 255, 255))
         for i in range(n):
             balls[i].move(0.5)
             pygame.draw.circle(game_display, ball_color,
-                               (balls[i].px, balls[i].py), balls[i].r)
+                               (balls[i].pos_x, balls[i].pos_y), balls[i].radius)
         pygame.display.update()
         time.sleep(0.01)
