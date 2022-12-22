33df58d5a258eae2e826c8ed878b08fcddc5dbd6
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2022-12-22 18:51:56 +0100

    Add typer for handling cli params in trie/graphics.py

--- data_structures/trie/graphics.py
+++ data_structures/trie/graphics.py
@@ -1,15 +1,23 @@
 """Module to generate visual representations of tries."""
 
+import typer
 
-from .implementation import Trie
+from implementation import Trie
 
-trie = Trie(["a", "b", "c", "d", "e"])
-trie.insert("abc")
-trie.insert("b")
-trie.insert("abc")
-trie.insert("abcde")
-trie.insert("bde")
-trie.insert("bdac")
 
-graph = trie.get_visual_representation()
-graph.write_png("data_structures/trie/output.png")
+def create_png(output_directory: str):
+
+    trie = Trie(["a", "b", "c", "d", "e"])
+    trie.insert("abc")
+    trie.insert("b")
+    trie.insert("abc")
+    trie.insert("abcde")
+    trie.insert("bde")
+    trie.insert("bdac")
+
+    graph = trie.get_visual_representation()
+    graph.write_png(output_directory)
+
+
+if __name__ == '__main__':
+    typer.run(create_png)
