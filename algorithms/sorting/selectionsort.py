22b4fb93ac58fc3a956891db7a9a60f3278cfbe1
Carlos Manuel González <cmglezpdev@gmail.com>
2023-01-09 06:34:05 +0100

    feat: add implementation of selection sort algorithm

---
+++ algorithms/sorting/selectionsort.py
@@ -0,0 +1,27 @@
+"""Module with the implementation of the SelectionSort algorithm."""
+
+
+from typing import List
+
+from .sort import Sort
+
+
+class SelectionSort(Sort):
+    """Class that represents a SelectionSort implementation."""
+
+    def _sort(self, items: List) -> List:
+        size = len(items)
+        sorted_items = items.copy()
+
+        for i in range(size):
+            min_index = i
+            for j in range(i + 1, size):
+                if self._comp_func(sorted_items[j], sorted_items[min_index]):
+                    min_index = j
+
+            sorted_items[i], sorted_items[min_index] = (
+                sorted_items[min_index],
+                sorted_items[i],
+            )
+
+        return sorted_items
