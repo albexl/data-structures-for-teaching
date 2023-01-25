f25ed1e0034b8c5e1395b93a539b80b88f686b34
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2023-01-25 22:24:26 +0100

    fix: Import issues on linked list client code

--- data_structures/linked_list/client.py
+++ data_structures/linked_list/client.py
@@ -1,6 +1,6 @@
 """Client code for LinkedList implementation."""
 
-from implementation import LinkedList
+from .implementation import LinkedList
 
 if __name__ == "__main__":
     linked_list = LinkedList()
@@ -12,15 +12,15 @@ if __name__ == "__main__":
     linked_list.add(4)
 
     print("Before insert")
-    print(f'Total items: {linked_list.length()}')
-    for i in range(1, linked_list.length()+1):
+    print(f"Total items: {linked_list.length()}")
+    for i in range(1, linked_list.length() + 1):
         print(linked_list.get(i))
 
     linked_list.insert(5, 2)
 
     print("After insert")
-    print(f'Total items: {linked_list.length()}')
-    for i in range(1, linked_list.length()+1):
+    print(f"Total items: {linked_list.length()}")
+    for i in range(1, linked_list.length() + 1):
         print(linked_list.get(i))
 
     aux = []
@@ -28,22 +28,22 @@ if __name__ == "__main__":
     print("Copy to...")
     for i in range(0, len(aux)):
         print(aux[i])
-    
-    print(f'Contain 2: {linked_list.contains(2)}')
-    print(f'Contain 6: {linked_list.contains(6)}')
-    print(f'Contain "Carlos": {linked_list.contains("Carlos")}')
-    print(f'Is Empty: {linked_list.is_empty()}')
 
-    print(f'Removing item 3: {linked_list.remove(3)}')
-    
+    print(f"Contain 2: {linked_list.contains(2)}")
+    print(f"Contain 6: {linked_list.contains(6)}")
+    print(f'Contain "Carlos": {linked_list.contains("Carlos")}')
+    print(f"Is Empty: {linked_list.is_empty()}")
+
+    print(f"Removing item 3: {linked_list.remove(3)}")
+
     print("After remove")
-    print(f'Total items: {linked_list.length()}')
-    for i in range(1, linked_list.length()+1):
+    print(f"Total items: {linked_list.length()}")
+    for i in range(1, linked_list.length() + 1):
         print(linked_list.get(i))
 
     linked_list.clear()
 
     print("After clear")
-    print(f'Total items: {linked_list.length()}')
-    for i in range(1, linked_list.length()+1):
+    print(f"Total items: {linked_list.length()}")
+    for i in range(1, linked_list.length() + 1):
         print(linked_list.get(i))
