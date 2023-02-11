15ee9294e8fedb23bff28fcb1ab4e39a218fbbdf
Ariel Plasencia Díaz <arielplasencia00@gmail.com>
2023-02-11 18:21:22 -0500

    fix: Change the logic of the test cases

--- tests/test_eratosthenes_sieve.py
+++ tests/test_eratosthenes_sieve.py
@@ -11,27 +11,28 @@ class TestEratosthenesSieve(TestCase):
     def setUp(self):
         self.n = 1000000
         self.eratosthenes_sieve = EratosthenesSieve(self.n)
-        self.no_cousins = self.eratosthenes_sieve.eratosthenes_sieve()
+        self.primes = self.eratosthenes_sieve.eratosthenes_sieve()
 
     def test_eratosthenes_sieve(self):
         """Check eratosthenes sieve algorithm."""
-        self.assertEqual(self.no_cousins[1], True)
-        self.assertEqual(self.no_cousins[101], False)
-        self.assertEqual(self.no_cousins[307], False)
-        self.assertEqual(self.no_cousins[863], False)
-        self.assertEqual(self.no_cousins[7735], True)
-        self.assertEqual(self.no_cousins[88643], False)
-        self.assertEqual(self.no_cousins[92567], False)
-        self.assertEqual(self.no_cousins[99991], False)
-        self.assertEqual(self.no_cousins[99999], True)
+        self.assertEqual(self.primes[1], False)
+        self.assertEqual(self.primes[101], True)
+        self.assertEqual(self.primes[307], True)
+        self.assertEqual(self.primes[863], True)
+        self.assertEqual(self.primes[7735], False)
+        self.assertEqual(self.primes[88643], True)
+        self.assertEqual(self.primes[92567], True)
+        self.assertEqual(self.primes[99991], True)
+        self.assertEqual(self.primes[99999], False)
 
     def test_get_count_cousins(self):
         """Check the count of prime numbers in an interval."""
-        self.assertEqual(self.eratosthenes_sieve.get_count_cousins(1, 100), 25)
-        self.assertEqual(self.eratosthenes_sieve.get_count_cousins(1, 1000), 168)
-        self.assertEqual(self.eratosthenes_sieve.get_count_cousins(100, 10000), 1204)
-        self.assertEqual(self.eratosthenes_sieve.get_count_cousins(1024, 1024), 0)
-        self.assertEqual(self.eratosthenes_sieve.get_count_cousins(1, 1000000), 78498)
-        self.assertEqual(self.eratosthenes_sieve.get_count_cousins(345, 874), 82)
-        self.assertEqual(self.eratosthenes_sieve.get_count_cousins(101, 101), 1)
-        self.assertEqual(self.eratosthenes_sieve.get_count_cousins(3762, 96108), 8737)
+        dp = self.eratosthenes_sieve.accum_primes()
+        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 1, 100), 25)
+        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 1, 1000), 168)
+        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 100, 10000), 1204)
+        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 1024, 1024), 0)
+        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 1, 1000000), 78498)
+        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 345, 874), 82)
+        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 101, 101), 1)
+        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 3762, 96108), 8737)
