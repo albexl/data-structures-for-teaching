09e53de18d98eba4a510e98ffa45299b13983e0c
Ariel Plasencia Díaz <arielplasencia00@gmail.com>
2023-02-11 18:13:03 -0500

    ref: Refactor eratosthenes sieve
    1. Rename all the no_cousins to no_primes.
    2. Change no_primes to primes and change the logic.
    3. Add more detailed docstring.

--- algorithms/number_theory/eratosthenes_sieve.py
+++ algorithms/number_theory/eratosthenes_sieve.py
@@ -1,4 +1,4 @@
-"""Module with the implementation of eratosthenes sieve."""
+"""Module with the implementation of eratosthenes sieve algorithm."""
 
 
 from typing import List
@@ -9,51 +9,65 @@ class EratosthenesSieve:
 
     def __init__(self, n: int):
         self._n = n
-        self._no_cousins = [False for _ in range(self._n + 1)]
+        self._primes = [True for _ in range(self._n + 1)]
 
     def eratosthenes_sieve(self) -> List[bool]:
         """
         Eratosthenes sieve algorithm.
+
+        Returns
+        -------
+        primes : List[bool]
+            Boolean array such that prime[i] = True if and only if i is prime.
         """
-        self._no_cousins[0] = self._no_cousins[1] = True
+        self._primes[0] = self._primes[1] = False
 
         for i in range(2, self._n + 1):
-            if not self._no_cousins[i]:
-                for j in range(i, self._n + 1):
-                    if i * j >= self._n + 1:
-                        break
-                    self._no_cousins[i * j] = True
+            if self._primes[i]:
+                for j in range(i + i, self._n + 1, i):
+                    self._primes[j] = False
 
-        return self._no_cousins
+        return self._primes
 
-    def _accum_cousins(self) -> List[int]:
+    def accum_primes(self) -> List[int]:
         """
-        Applies acumulative sum according to the no_cousins array.
+        Applies acumulative sum according to the primes array.
+
+        Returns
+        -------
+        dp : List[int]
+            Acumulative sum according to the primes array.
         """
         dp = [0 for _ in range(self._n + 1)]
         for i in range(1, self._n + 1):
             dp[i] = dp[i - 1]
-            if not self._no_cousins[i]:
+            if self._primes[i]:
                 dp[i] += 1
         return dp
 
-    def get_count_cousins(self, left: int, right: int) -> int:
+    def get_count(self, dp: List[int], left: int, right: int) -> int:
         """
         Returns the count of prime numbers in the interval [left, right].
 
         Parameters
         ----------
+        dp : List[int]
+            Acumulative sum according to the primes array.
         left : int
             Lower limit of range.
         right : int
             Upper limit of range.
+
+        Returns
+        -------
+        count_primes : int
+            Count of prime numbers in the interval [left, right].
         """
-        count_cousins = 0
-        dp = self._accum_cousins()
+        count_primes = 0
 
         if left == 0:
-            count_cousins = dp[right]
+            count_primes = dp[right]
         else:
-            count_cousins = dp[right] - dp[left - 1]
+            count_primes = dp[right] - dp[left - 1]
 
-        return count_cousins
+        return count_primes
