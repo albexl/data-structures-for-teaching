63b70cac703e28c6eb6a7fdc162452ded4fc1c5f
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2023-02-12 11:56:13 +0100

    fix: Update interface and code styling for eratosthenes sieve implementation

--- tests/test_eratosthenes_sieve.py
+++ tests/test_eratosthenes_sieve.py
@@ -9,30 +9,27 @@ class TestEratosthenesSieve(TestCase):
     """Class to test the eratosthenes sieve implementation."""
 
     def setUp(self):
-        self.n = 1000000
-        self.eratosthenes_sieve = EratosthenesSieve(self.n)
-        self.primes = self.eratosthenes_sieve.eratosthenes_sieve()
+        self.prime_finder = EratosthenesSieve(1000000)
 
-    def test_eratosthenes_sieve(self):
+    def test_primality_check(self):
         """Check eratosthenes sieve algorithm."""
-        self.assertEqual(self.primes[1], False)
-        self.assertEqual(self.primes[101], True)
-        self.assertEqual(self.primes[307], True)
-        self.assertEqual(self.primes[863], True)
-        self.assertEqual(self.primes[7735], False)
-        self.assertEqual(self.primes[88643], True)
-        self.assertEqual(self.primes[92567], True)
-        self.assertEqual(self.primes[99991], True)
-        self.assertEqual(self.primes[99999], False)
+        self.assertEqual(self.prime_finder.is_prime(1), False)
+        self.assertEqual(self.prime_finder.is_prime(101), True)
+        self.assertEqual(self.prime_finder.is_prime(307), True)
+        self.assertEqual(self.prime_finder.is_prime(863), True)
+        self.assertEqual(self.prime_finder.is_prime(7735), False)
+        self.assertEqual(self.prime_finder.is_prime(88643), True)
+        self.assertEqual(self.prime_finder.is_prime(92567), True)
+        self.assertEqual(self.prime_finder.is_prime(99991), True)
+        self.assertEqual(self.prime_finder.is_prime(99999), False)
 
-    def test_get_count_cousins(self):
+    def test_get_primes_count(self):
         """Check the count of prime numbers in an interval."""
-        dp = self.eratosthenes_sieve.accum_primes()
-        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 1, 100), 25)
-        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 1, 1000), 168)
-        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 100, 10000), 1204)
-        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 1024, 1024), 0)
-        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 1, 1000000), 78498)
-        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 345, 874), 82)
-        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 101, 101), 1)
-        self.assertEqual(self.eratosthenes_sieve.get_count(dp, 3762, 96108), 8737)
+        self.assertEqual(self.prime_finder.get_primes_count(1, 100), 25)
+        self.assertEqual(self.prime_finder.get_primes_count(1, 1000), 168)
+        self.assertEqual(self.prime_finder.get_primes_count(100, 10000), 1204)
+        self.assertEqual(self.prime_finder.get_primes_count(1024, 1024), 0)
+        self.assertEqual(self.prime_finder.get_primes_count(1, 1000000), 78498)
+        self.assertEqual(self.prime_finder.get_primes_count(345, 874), 82)
+        self.assertEqual(self.prime_finder.get_primes_count(101, 101), 1)
+        self.assertEqual(self.prime_finder.get_primes_count(3762, 96108), 8737)
