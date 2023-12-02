30f0ac06758fa3fb4fe3fe50214fdcd4a95a6511
Alberto Gonzalez Rosales <albexxl27@gmail.com>
2023-12-02 21:26:06 +0100

    Add minor improvements to eratosthenes sieve implementation

--- algorithms/number_theory/eratosthenes_sieve.py
+++ algorithms/number_theory/eratosthenes_sieve.py
@@ -1,12 +1,9 @@
-"""Module with the implementation of eratosthenes sieve algorithm."""
-
-
 class EratosthenesSieve:
-    """Class implementation of the eratosthenes sieve algorithm."""
+    """Class implementation of the Eratosthenes sieve algorithm."""
 
     def __init__(self, n: int):
         self._n = n
-        self._primes = [True for _ in range(self._n + 1)]
+        self._primes = [True] * (self._n + 1)
         self._compute_primes()
         self._accumulate_primes()
 
@@ -15,30 +12,28 @@ class EratosthenesSieve:
         self._primes[0] = self._primes[1] = False
         for i in range(2, self._n + 1):
             if self._primes[i]:
-                for j in range(i + i, self._n + 1, i):
+                for j in range(i * i, self._n + 1, i):
                     self._primes[j] = False
 
     def _accumulate_primes(self):
         """Applies cumulative sum to the primes array."""
-        self.cum_sum = [0 for _ in range(self._n + 1)]
+        self.cum_sum = [0] * (self._n + 1)
         for i in range(1, self._n + 1):
             self.cum_sum[i] = self.cum_sum[i - 1] + int(self._primes[i])
 
     def get_primes_count(self, left: int, right: int) -> int:
         """Returns the count of prime numbers in the interval [left, right].
 
-        Parameters
-        ----------
-        left : int
-            Lower limit of range.
-        right : int
-            Upper limit of range.
+        Args:
+            left (int): Lower limit of the range.
+            right (int): Upper limit of the range.
 
-        Returns
-        -------
-        count_primes : int
-            Count of prime numbers in the interval [left, right].
+        Returns:
+            int: Count of prime numbers in the interval [left, right].
         """
+        if left < 0 or right < 0 or left > right:
+            raise ValueError("Invalid range")
+
         return self.cum_sum[right] - (self.cum_sum[left - 1] if left > 0 else 0)
 
     def is_prime(self, number: int) -> bool:
